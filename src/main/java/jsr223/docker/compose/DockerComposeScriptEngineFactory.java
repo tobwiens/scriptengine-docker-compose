71eb15cc8f797bed68960a7a3f2e80dbf71e8e4b
Tobias Wiens <tobias.wiens@activeeon.com>
2015-04-22 11:37:42 +0200

    Create static initialization for the docker compose script engine factory. It will read an external configuration file, when loaded into memory.

--- src/main/java/jsr223/docker/compose/DockerComposeScriptEngineFactory.java
+++ src/main/java/jsr223/docker/compose/DockerComposeScriptEngineFactory.java
@@ -1,34 +1,93 @@
 package jsr223.docker.compose;
 
 
+import jsr223.docker.compose.utils.DockerComposeUtilities;
+import processbuilder.SingletonProcessBuilderFactory;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class DockerComposeScriptEngineFactory implements ScriptEngineFactory {
 
+    // External configuration
+    private static final String DOCKER_COMPOSE_CONFIGURATION_FILE_PATH = "config/docker-compose.properties";
+
+    private static Properties properties = new Properties();
+    private static InputStream input = null;
+
+    // Configuration keys
+    private static final String PROP_DOCKER_COMPOSE_COMMAND = "docker.compose.command";
+    private static final String PROP_DOCKER_COMPOSE_COMMAND_DEFAULT = "/usr/local/bin/docker-compose";
+    private static final String PROP_SUDO_COMMAND = "sudo.command";
+    private static final String PROP_SUDO_COMMAND_DEFAULT = "/usr/bin/sudo";
+    private static final String PROP_DOCKER_COMPOSE_USE_SUDO = "docker.compose.use.sudo";
+    private static final String PROP_DOCKER_COMPOSE_USE_SUDO_DEFAULT = "false";
+
+    // Docker compose configuration - are initialized when loaded into memory; static { }.
+    public static String dockerComposeCommand;
+    public static String sudoCommand;
+    public static boolean useSudo;
+
+    // Script engine parameters
     private static final String NAME = "docker-compose";
     private static final String ENGINE = "docker-compose yaml file";
     private static final String ENGINE_VERSION = ""; // TODO: Create versions
-    private static final String LANGUAGE = "Bash"; // TODO: correct language
-    private static final String LANGUAGE_VERSION = "not existing"; // TODO: Add correct version
+    private static final String LANGUAGE = "yaml"; // TODO: correct language
 
     private static final Map<String, Object> parameters = new HashMap<String, Object>();
 
+    // TODO: Read command from config file
     static {
-        parameters.put(ScriptEngine.NAME, NAME);
-        parameters.put(ScriptEngine.ENGINE, ENGINE);
-        parameters.put(ScriptEngine.ENGINE_VERSION, ENGINE_VERSION);
-        parameters.put(ScriptEngine.LANGUAGE, LANGUAGE);
-        parameters.put(ScriptEngine.LANGUAGE_VERSION, LANGUAGE_VERSION);
+
+        try {
+            // Open configuration file
+            input = new FileInputStream(DOCKER_COMPOSE_CONFIGURATION_FILE_PATH);
+            // Load fields
+            properties.load(input);
+
+
+
+        } catch (java.io.IOException e) {
+            // No configuration file
+            System.err.println("No configuration file for docker-compose script engine.");
+            System.err.println("Use standard values, execution might not work.");
+        } finally {
+
+            // Load properties
+            DockerComposeScriptEngineFactory.dockerComposeCommand =
+                    properties.getProperty(PROP_DOCKER_COMPOSE_COMMAND,
+                            PROP_DOCKER_COMPOSE_COMMAND_DEFAULT);
+            DockerComposeScriptEngineFactory.sudoCommand =
+                    properties.getProperty(PROP_SUDO_COMMAND,
+                            PROP_SUDO_COMMAND_DEFAULT);
+            DockerComposeScriptEngineFactory.useSudo =
+                    Boolean.parseBoolean(properties.getProperty(PROP_DOCKER_COMPOSE_USE_SUDO,
+                            PROP_DOCKER_COMPOSE_USE_SUDO_DEFAULT));
+
+
+            // Write script engine parameters
+            parameters.put(ScriptEngine.NAME, NAME);
+            parameters.put(ScriptEngine.ENGINE, ENGINE);
+            parameters.put(ScriptEngine.ENGINE_VERSION, ENGINE_VERSION);
+            parameters.put(ScriptEngine.LANGUAGE, LANGUAGE);
+            parameters.put(ScriptEngine.LANGUAGE_VERSION, DockerComposeUtilities.
+                    getDockerComposeVersion(DockerComposeScriptEngineFactory.dockerComposeCommand,
+                            SingletonProcessBuilderFactory.getInstance()));
+        }
+
+
     }
 
-    // TODO: Implement
+
     @Override
     public String getEngineName() {
-        return null;
+        return (String) parameters.get(ScriptEngine.NAME);
     }
     // TODO: Implement
     @Override
